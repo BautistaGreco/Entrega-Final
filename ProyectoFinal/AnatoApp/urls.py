a908abf59ebd948eaa7c5e8c535f03751b86ef63
BautistaGreco <bautistagreco@gmail.com>
2023-05-15 16:17:33 -0300

    Registro

--- ProyectoFinal/AnatoApp/urls.py
+++ ProyectoFinal/AnatoApp/urls.py
@@ -4,5 +4,6 @@ from AnatoApp import views
 urlpatterns = [
    
     path('', views.inicio, name="Inicio"),
+    path('registro', views.register, name='Registro'),
 
 ]
--- ProyectoFinal/AnatoApp/views.py
+++ ProyectoFinal/AnatoApp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def inicio (request):
     return render (request, 'AnatoApp/inicio.html')
+
+
+from AnatoApp.forms import UserRegisterForm
+def register(request):
+      if request.method == 'POST':
+            #form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"AnatoApp/inicio.html" ,  {"mensaje":"Usuario Creado :)"})
+      else:
+            #form = UserCreationForm()       
+            form = UserRegisterForm()     
+      return render(request,"AnatoApp/registro.html" ,  {"form":form})
