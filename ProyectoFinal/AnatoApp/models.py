08bdad26b7a3f501f2b68426172094df6c860c54
BautistaGreco <bautistagreco@gmail.com>
2023-05-17 01:39:47 -0300

    registro fallando

--- ProyectoFinal/AnatoApp/models.py
+++ ProyectoFinal/AnatoApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 
 #####FALTA MAKEMIGRATIONS
@@ -7,7 +7,11 @@ from django.db import models
 
 
 # Create your models here.
-class Alumno(models.Model):
+from django.contrib.auth.models import AbstractUser
+
+class Alumno(AbstractUser):
+    password = models.CharField(max_length=128, default='valor_predeterminado')
+    username = models.CharField(max_length=150, unique=True, default='asd')
     def __str__(self):
         return f"Nombre: {self.nombre} - Comision {self.comision}"
     nombre = models.CharField(max_length=40)
@@ -38,4 +42,4 @@ class Entrada(models.Model):
     subtitulo = models.CharField(max_length=100)
     cuerpo = models.TextField()
     fecha = models.DateField(auto_now_add=True) 
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)  
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
